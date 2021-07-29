c21084ecf52d76e9568864e0e21159f6ff17b41b
carter-willey <cjwilley23@gmail.com>
2021-07-29 13:36:14 -0500

    reverted code to basic structure to start from scratch. Added wireframe image

--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import Flashcard from './Flashcard.js'
 
-export default function FlashcardList({ flashcards }) {
-    // ({ flashcards }) replaces doing props.flashcards
-    return (
-        <div className="card-grid">
-            { flashcards.map(card => {
-                return <Flashcard flashcard={ card } key={ card.id } />
-            }) }
-            {
-            /* When react goes to rerender the array of  
-                flashcard components, it will only rerender
-                the ones that will actually change in 
-                the flashcard array. This is why card.id
-                will be passed into Flashcard.js 
-            */}
-        </div>
-    )
+const FlashcardList = (props) => {
+    return ( 
+        <h1>Hello</h1>
+     );
 }
+ 
+export default FlashcardList;
