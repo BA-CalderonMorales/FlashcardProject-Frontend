7423a5ad4599d2f79ce17557101d08efea46651d
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-02 10:57:31 -0500

    Added UseForm and AddFlashcard component. Working on logic for their jsx files.

---
+++ src/Components/UseForm/useForm.jsx
@@ -0,0 +1,31 @@
+import React,{ useState } from 'react';
+
+export default function UseForm({callback}) {
+    // Hook for state variable and function to update state variable.
+    const[values, setValues] = useState([]);
+
+
+    const handleChange = (event) => {
+        // Calling event.persist() allows references to the event 
+        // to occur asynchronously.
+        event.persist();
+
+        // ...values (previous values) spread syntax.
+        setValues(values => ( 
+            {
+                ...values, 
+                [event.target.value]: event.target.values
+            } 
+        ));
+    }
+
+    const handleSubmit = (event) => {
+        // prevent default behavior of onSubmit, which refreshes page.
+        event.preventDefault();
+
+        // callback function passed in through props.
+        callback();
+    }
+
+    return { values, handleChange, handleSubmit }
+}
