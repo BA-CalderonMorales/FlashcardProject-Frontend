a83b1acb524790b8bc7843a1fde17172501630fa
carter-willey <cjwilley23@gmail.com>
2021-08-03 13:07:33 -0500

    added design/css to the collections and flashcards

--- src/Components/Flashcard/Flashcard.js
+++ src/Components/Flashcard/Flashcard.js
@@ -1,17 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import UpdateFlashcard from '../UpdateFlashcard/UpdateFlashcard';
+import './Flashcard.css'
 
 const Flashcard = ({card, index, amountOfCards}) => {
     const [editButtonClicked, setEditButtonClicked] = useState(false);
+    const[flip, setFlip] = useState(false);
 
     return ( 
         <>
-        
+            <div className={ `flashcard ${flip ? 'flip' : '' }` } 
+            onClick={ () => setFlip(!flip)} 
+            >
+                {flip ? 
+                <div className="back" >
+                    { card.back_content }
+                </div>
+                :
+                <div className="front" >
+                    { card.front_content }
+                </div>
+                }
+                 
+                <div className="index">{index + 1}/{amountOfCards}</div>    
+            </div>
             
-            <p>Back Content: {card.back_content}</p>
-            <p>Front Content: {card.front_content}</p>
-            <p>Deck Id: {card.deck}</p>
-            <p>{index + 1}/{amountOfCards}</p>
+            
             <button onClick={() => setEditButtonClicked(true)} >Edit</button>
             {editButtonClicked ? <UpdateFlashcard card_id={card.id} /> : <> </>}
         </>  
