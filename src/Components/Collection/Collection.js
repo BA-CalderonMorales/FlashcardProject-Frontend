d8965a7b2fe7b8c826b59d1fb3fe809d9911a812
carter-willey <cjwilley23@gmail.com>
2021-08-04 11:19:00 -0500

    fixed bug where multiple collections could be highlighted at once

--- src/Components/Collection/Collection.js
+++ src/Components/Collection/Collection.js
@@ -4,12 +4,13 @@ import './Collection.css';
 const Collection = (props) => {
 
   const [isActive, setIsActive] = useState(false);
+  
 
   return ( 
     <>
       <div className="row row-cols-1 p-2">
         <div className="col p-2" >
-          <div class={`collection ${isActive ? "selected" : ''}`} onClick={()=> setIsActive(!isActive) }>
+          <div className={`collection ${(props.collection.id == props.collectionClickedId) ? "selected" : ''}`} onClick={()=> setIsActive(!isActive) }>
             {props.collection.name}
           </div>
         </div>
--- src/Components/CollectionList/CollectionList.js
+++ src/Components/CollectionList/CollectionList.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Collection from '../Collection/Collection';
 import './CollectionList.css';
 
 const CollectionList = (props) => {
+  const [collectionClickedId, setCollectionClickedId] = useState(null);
 
   return ( 
     <div className="collection-list">
       { props.collections.map(collection =>{
         return (
-          <div onClick={() => {props.setCollectionId(collection.id); props.setIndex(0); props.setIsCollectionClicked(true)}}  key={collection.id}>
-            <Collection collection={collection} />
+          <div key={collection.id} onClick={() => {props.setCollectionId(collection.id); setCollectionClickedId(collection.id); props.setIndex(0); props.setIsCollectionClicked(true); }}>
+            <Collection collection={collection} collectionClickedId={collectionClickedId} />
           </div>
         )
       })}
